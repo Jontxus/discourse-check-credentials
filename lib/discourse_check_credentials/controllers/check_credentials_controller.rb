dcd7350f7b505c61ca1a5bec919b083eff2d6d64
Jon <jmartinez@triplejsoftware.es>
2024-12-28 07:51:48 +0100

    User authenticator

--- lib/discourse_check_credentials/controllers/check_credentials_controller.rb
+++ lib/discourse_check_credentials/controllers/check_credentials_controller.rb
@@ -24,15 +24,16 @@ module ::DiscourseCheckCredentials
         
             user = User.find_by_username_or_email(username)
         
-            if user && user.password_correct?(password)
-                Rails.logger.info("[CheckCredentials] IP=#{request.ip} username=#{username} -> OK")
-        
-                render json: {
+            if user
+                authenticator = UserAuthenticator.new(user, {})
+                if authenticator.authenticate(password)
+                  Rails.logger.info("[CheckCredentials] IP=#{request.ip} username=#{username} -> OK")
+                  return render json: {
                     valid: true,
                     user_id: user.id,
                     username: user.username,
                     email: user.email,
-                }
+                  }
             else
                 Rails.logger.warn("[CheckCredentials] IP=#{request.ip} username=#{username} -> Credenciales inválidas")
                 render json: { valid: false }, status: 401
