3879ba9ac5c178ecc7c0ef28a09a2eaa6e089609
Jon <jmartinez@triplejsoftware.es>
2024-12-28 06:29:34 +0100

    Logger init

--- plugin.rb
+++ plugin.rb
@@ -4,8 +4,12 @@
 # authors: "Jon"
 # url: https://github.com/Jontxus/discourse-check-credentials
 
+# plugin.rb
+Rails.logger.info(">>> Entrando a plugin.rb de discourse-check-credentials <<<")
 enabled_site_setting :check_credentials_enabled
 
 after_initialize do
+  Rails.logger.info(">>> after_initialize del plugin discourse-check-credentials <<<")
+
   load File.expand_path('../lib/discourse_check_credentials/engine.rb', __FILE__)
 end
