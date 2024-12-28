48d83474e0a9f85ad097b605f16df6d1c47657ae
Jon <jmartinez@triplejsoftware.es>
2024-12-28 07:05:19 +0100

    Info correct

--- plugin.rb
+++ plugin.rb
@@ -5,11 +5,10 @@
 # url: https://github.com/Jontxus/discourse-check-credentials
 
 # plugin.rb
-Rails.logger.info(">>> Entrando a plugin.rb de discourse-check-credentials <<<")
+
 enabled_site_setting :check_credentials_enabled
 
 after_initialize do
-  Rails.logger.info(">>> after_initialize del plugin discourse-check-credentials <<<")
 
   load File.expand_path('../lib/discourse_check_credentials/engine.rb', __FILE__)
 end
